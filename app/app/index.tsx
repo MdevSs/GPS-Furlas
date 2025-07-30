81aa65a85fad2cd8dcf80e66933fbc73a9c8a6d5
MdevSs <you@example.comleandrohenrique.denise@gmail.com>
2025-07-29 21:17:41 -0300

    Add needed Packages and start develop

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.main}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    main:{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+    map: { flex: 1 },
+});
